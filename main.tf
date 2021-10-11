1fa10f470750909e02c922a74be35f6daa87a1db
JLiu <jliu@happymoney.com>
2021-10-11 11:17:40 -0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,5 @@
+locals {
+  alb = templatefile("/templates/alb.json", {
+    
+  })
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "alb" {
+  description = "ALB map"
+  default     = {}
+}
+
+variable "nlb" {
+  description = "NLB map"
+  default     = {}
+}
+
+variable "apigatewayv2" {
+  description = "Api Gateway v2 map"
+  default     = {}
+}
