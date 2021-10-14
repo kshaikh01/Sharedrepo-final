40e534640218685cefa8dcbae29a7cb8de77e5c5
Shun Ochiai <sochiai@happymoney.com>
2021-10-13 17:16:03 -0700

    test

--- test/inspec/controls/test.rb
+++ test/inspec/controls/test.rb
@@ -4,6 +4,9 @@ json_file = inspec.profile.file(file_name)
 attributes = JSON.parse(json_file)
 
 id = attributes['id']
+mock_alb = attributes['mock_alb']
+mock_nlb = attributes['mock_nlb']
+mock_apigatewayv2 = attributes['mock_apigatewayv2']
 monitors = attributes['monitors']
 empty_monitors = attributes['empty_monitors']
 
@@ -15,4 +18,8 @@ control 'datadog_catalog' do
     describe empty_monitors.empty? do
         it { should cmp true }
     end
+
+    describe monitors do
+        its("\"#{id}/alb/httpcode_elb_5xx\".name") { should cmp "HTTPCode_elb_5XX: #{id}" }
+    end    
 end  
