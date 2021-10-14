a59b59b34a1d051921b56fcf8188cc0598807d65
Shun Ochiai <sochiai@happymoney.com>
2021-10-14 15:18:44 -0700

    Add tests for nlb and apigateway monitors

--- test/inspec/controls/test.rb
+++ test/inspec/controls/test.rb
@@ -12,7 +12,15 @@ empty_monitors = attributes['empty_monitors']
 
 comparisons = {
     monitors["#{id}/alb/httpcode_elb_5xx"]["name"] => "HTTPCode_elb_5XX: #{mock_alb}",
-    monitors["#{id}/alb/request_count"]["name"] => "request_count_override: #{mock_alb}"
+    monitors["#{id}/alb/request_count"]["name"] => "request_count_override: #{mock_alb}", # Test override
+    monitors["#{id}/alb/target_connection_error_count"]["name"] => "target_connection_error_count: #{mock_alb}",
+    monitors["#{id}/alb/target_response_time"]["name"] => "target_response_time: #{mock_alb}",
+    monitors["#{id}/custom_alb"]["name"] => "custom_target_connection_error_count: #{mock_alb}", # Test additional custom monitor
+    monitors["#{id}/nlb/active_flow_count"]["name"] => "active_flow_count: #{mock_nlb}",
+    monitors["#{id}/nlb/processed_bytes"]["name"] => "processed_bytes: #{mock_nlb}",
+    monitors["#{id}/nlb/tcpelbreset_count"]["name"] => "tcpelbreset_count: #{mock_nlb}",
+    monitors["#{id}/apigatewayv2/4xx"]["name"] => "4xx: #{mock_nlb}",
+    monitors["#{id}/apigatewayv2/5xx"]["name"] => "5xx: #{mock_apigatewayv2}"
 }
 
 #--------------------------------------
