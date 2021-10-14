49a5c9735e04a438ff812e3f483efa98d8e1e582
Shun Ochiai <sochiai@happymoney.com>
2021-10-13 17:32:47 -0700

    Test

--- test/inspec/controls/test.rb
+++ test/inspec/controls/test.rb
@@ -2,12 +2,13 @@
 file_name = "output.json"
 json_file = inspec.profile.file(file_name)
 attributes = JSON.parse(json_file)
+attributes_struct = JSON.parse(json_file, object_class: OpenStruct)
 
 id = attributes['id']
 mock_alb = attributes['mock_alb']
 mock_nlb = attributes['mock_nlb']
 mock_apigatewayv2 = attributes['mock_apigatewayv2']
-monitors = attributes['monitors']
+monitors = attributes_struct.monitors
 empty_monitors = attributes['empty_monitors']
 
 #--------------------------------------
